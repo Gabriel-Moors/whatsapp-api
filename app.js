e57bdfae76eef8dc67c5f404516e3dc141d16266
Gabriel Moraes <134667688+Gabriel-Moors@users.noreply.github.com>
2023-05-28 00:38:00 -0300

    Update app.js

--- app.js
+++ app.js
@@ -16,19 +16,23 @@ app.post('/sessions', async (req, res) => {
 
   try {
     if (!sessions[sessionId]) {
-      sessions[sessionId] = await venom.create(sessionId);
+      const instance = await venom.create(sessionId);
 
-      // Gerar o QR Code para a nova instância
-      const qrCode = await sessions[sessionId].getQrCode();
+      instance.onStateChange((state) => {
+        if (state === 'CONNECTED') {
+          // Enviar a resposta com o QR Code e uma mensagem de sucesso
+          const qrCode = instance.base64QrCode;
+          res.status(200).json({ message: 'Sessão criada com sucesso.', qrCode });
+        }
+      });
 
-      // Enviar a resposta com o QR Code e uma mensagem de sucesso
-      return res.status(200).json({ message: 'Sessão criada com sucesso.', qrCode });
+      sessions[sessionId] = instance;
     } else {
-      return res.status(200).json({ message: 'Sessão já existe.' });
+      res.status(200).json({ message: 'Sessão já existe.' });
     }
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ error: 'Falha ao criar a sessão.' });
+    res.status(500).json({ error: 'Falha ao criar a sessão.' });
   }
 });
 
@@ -37,10 +41,11 @@ app.delete('/sessions/:sessionId', (req, res) => {
   const { sessionId } = req.params;
 
   if (sessions[sessionId]) {
+    sessions[sessionId].close();
     delete sessions[sessionId];
-    return res.status(200).json({ message: 'Sessão excluída com sucesso.' });
+    res.status(200).json({ message: 'Sessão excluída com sucesso.' });
   } else {
-    return res.status(404).json({ error: 'Sessão não encontrada.' });
+    res.status(404).json({ error: 'Sessão não encontrada.' });
   }
 });
 
@@ -53,10 +58,10 @@ app.post('/sessions/:sessionId/send-message', async (req, res) => {
     const session = sessions[sessionId];
     await session.sendText(number, message);
 
-    return res.status(200).json({ message: 'Mensagem enviada com sucesso.' });
+    res.status(200).json({ message: 'Mensagem enviada com sucesso.' });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ error: 'Falha ao enviar a mensagem.' });
+    res.status(500).json({ error: 'Falha ao enviar a mensagem.' });
   }
 });
 
