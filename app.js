459f814f03408748ba266bb3e98a12477afed134
Gabriel Moraes <134667688+Gabriel-Moors@users.noreply.github.com>
2023-05-28 00:56:39 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,54 +1,89 @@
-const express = require('express');
 const venom = require('venom-bot');
+const express = require('express');
+const winston = require('winston');
 
 const app = express();
 app.use(express.json());
 
-let sessionInstance;
+const sessions = {}; // Armazenará as instâncias do Venom
 
-// Endpoint para criar uma nova instância do Venom-bot
+// Endpoint para criar uma nova instância
 app.post('/sessions', async (req, res) => {
+  const { sessionId } = req.body;
+
+  if (!sessionId) {
+    return res.status(400).json({ error: 'O ID da sessão é obrigatório.' });
+  }
+
   try {
-    const { sessionId } = req.body;
+    if (!sessions[sessionId]) {
+      sessions[sessionId] = await venom.create(sessionId);
 
-    if (!sessionId) {
-      return res.status(400).json({ error: 'O ID da sessão é obrigatório.' });
+      // Gerar o QR Code para a nova instância
+      const qrCode = await sessions[sessionId].getQrCode();
+
+      // Enviar a resposta com o QR Code
+      return res.status(200).json({ message: 'Sessão criada com sucesso.', qrCode });
+    } else {
+      return res.status(200).json({ message: 'Sessão já existe.' });
     }
-
-    if (sessionInstance) {
-      return res.status(400).json({ error: 'Já existe uma sessão em execução.' });
-    }
-
-    sessionInstance = await venom.create(sessionId, (base64QrCode) => {
-      // Callback para receber o QR Code
-      const qrCode = `data:image/png;base64, ${base64QrCode}`;
-      res.status(200).json({ message: 'Sessão criada com sucesso.', qrCode });
-    });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Falha ao criar a sessão.' });
+    return res.status(500).json({ error: 'Falha ao criar a sessão.' });
+  }
+});
+
+// Endpoint para excluir uma instância
+app.delete('/sessions/:sessionId', (req, res) => {
+  const { sessionId } = req.params;
+
+  if (sessions[sessionId]) {
+    delete sessions[sessionId];
+    return res.status(200).json({ message: 'Sessão excluída com sucesso.' });
+  } else {
+    return res.status(404).json({ error: 'Sessão não encontrada.' });
   }
 });
 
 // Endpoint para enviar uma mensagem
 app.post('/sessions/:sessionId/send-message', async (req, res) => {
+  const { sessionId } = req.params;
+  const { number, message } = req.body;
+
   try {
-    const { sessionId } = req.params;
-    const { number, message } = req.body;
+    const session = sessions[sessionId];
+    await session.sendText(number, message);
 
-    if (!sessionInstance) {
-      return res.status(400).json({ error: 'Nenhuma sessão ativa.' });
-    }
-
-    await sessionInstance.sendText(number, message);
-    res.status(200).json({ message: 'Mensagem enviada com sucesso.' });
+    return res.status(200).json({ message: 'Mensagem enviada com sucesso.' });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Falha ao enviar a mensagem.' });
+    return res.status(500).json({ error: 'Falha ao enviar a mensagem.' });
   }
 });
 
-// Iniciar o servidor
+// Configurar logs
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.simple(),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' })
+  ]
+});
+
+// Registrar solicitações recebidas nos logs
+app.use((req, res, next) => {
+  logger.info(`${req.method} ${req.url}`);
+  next();
+});
+
+// Configurar rota padrão
+app.use((req, res) => {
+  return res.status(404).json({ error: 'Endpoint não encontrado.' });
+});
+
+// Criar servidor HTTP
 const port = 80;
 app.listen(port, () => {
   console.log(`Servidor em execução na porta ${port}`);
