aab20dcd632579f48b357c188288a4a7c108aef9
Gabriel Moraes <134667688+Gabriel-Moors@users.noreply.github.com>
2023-05-28 11:47:52 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 80; // Porta 80
 
 app.get('/', (req, res) => {
-  res.send('API do WhatsApp');
+  res.json({ message: 'API do WhatsApp' });
 });
 
 app.get('/qr', (req, res) => {
@@ -14,22 +14,19 @@ app.get('/qr', (req, res) => {
     .then((client) => {
       client.onStateChange((state) => {
         if (state.qrcode && state.status === 'CONFLICT') {
-          // Retorna o código QR como uma resposta JSON
           res.json({ qrcode: state.qrcode });
         } else if (state.status === 'CONNECTED') {
-          // Instância conectada com sucesso
-          res.send('Instância conectada com sucesso!');
+          res.json({ message: 'Instância conectada com sucesso!' });
         }
       });
 
       client.onAnyMessage((message) => {
-        // Manipula todas as mensagens recebidas
         console.log(message);
       });
     })
     .catch((error) => {
       console.error('Erro ao criar o cliente Venom:', error);
-      res.status(500).send('Erro ao criar o cliente Venom');
+      res.status(500).json({ error: 'Erro ao criar o cliente Venom' });
     });
 });
 
