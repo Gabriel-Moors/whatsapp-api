473ff1d052b5e0cc4f98c1966ad86812ce84f582
Gabriel Moraes <134667688+Gabriel-Moors@users.noreply.github.com>
2023-05-28 11:31:54 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,39 +2,32 @@ const express = require('express');
 const venom = require('venom-bot');
 
 const app = express();
-const port = 80; // Alterado para a porta 80
+const port = 80; // Porta 80
 
-// Configuração básica do servidor express
 app.get('/', (req, res) => {
   res.send('API do WhatsApp');
 });
 
-// Endpoint para gerar o código QR
 app.get('/qr', (req, res) => {
   venom
     .create()
     .then((client) => {
       client.onStateChange((state) => {
-        // Verifica se o estado é 'CONFLICT' ou 'UNLAUNCHED'
-        if (state === venom.SocketState.CONFLICT || state === venom.SocketState.UNLAUNCHED) {
-          res.status(500).send('Erro ao gerar o código QR');
+        if (state.qrcode) {
+          // Converte o código QR em base64
+          const qrCodeBase64 = state.qrcode.replace('data:image/png;base64,', '');
+
+          // Retorna o código QR como uma resposta JSON
+          res.json({ qrcode: qrCodeBase64 });
         }
       });
 
-      client.onQrCode((qrCode) => {
-        // Retorna o código QR como uma imagem base64
-        const qrCodeImage = `<img src="data:image/png;base64, ${qrCode}" alt="QR Code">`;
-        res.send(qrCodeImage);
-      });
-
       client.onReady(() => {
-        // A instância do WhatsApp Web está pronta para uso
         console.log('Pronto para usar');
-        client.close(); // Fecha o cliente após obter o código QR
+        client.close();
       });
 
       client.onAnyMessage((message) => {
-        // Manipula todas as mensagens recebidas
         console.log(message);
       });
     })
@@ -44,7 +37,6 @@ app.get('/qr', (req, res) => {
     });
 });
 
-// Inicia o servidor
 app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+  console.log(`Servidor rodando na porta ${port}`);
 });
