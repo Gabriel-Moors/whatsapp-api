daeee14c0070a6a39b09af36de7ef6a61a48246f
Gabriel Moraes <134667688+Gabriel-Moors@users.noreply.github.com>
2023-05-28 12:25:08 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,19 +5,50 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-venom.create().then((client) => {
-  app.post('/sendMessage', (req, res) => {
-    const { number, message } = req.body;
-    client.sendText(number, message).then((result) => {
-      res.json({ success: true, message: 'Message sent successfully' });
-    }).catch((error) => {
-      res.status(500).json({ success: false, message: 'Failed to send message', error });
-    });
-  });
+const instances = [];
 
-  app.listen(80, () => {
-    console.log('API running on port 80');
+app.post('/createInstance', (req, res) => {
+  venom.create().then((client) => {
+    instances.push(client);
+    const qrcode = client.getQRCode();
+    res.status(200).json({ success: true, message: 'Escaneie o QR code para conectar', qrcode });
+  }).catch((error) => {
+    res.status(500).json({ success: false, message: 'Falha ao criar instância', error });
   });
-}).catch((error) => {
-  console.log('Failed to create venom client', error);
+});
+
+app.post('/sendMessage', (req, res) => {
+  const { instanceIndex, number, message } = req.body;
+  const client = instances[instanceIndex];
+  if (!client) {
+    res.status(404).json({ success: false, message: 'Instância não encontrada' });
+    return;
+  }
+  client.sendText(number, message).then((result) => {
+    res.sendStatus(200);
+  }).catch((error) => {
+    res.status(500).json({ success: false, message: 'Falha ao enviar mensagem', error });
+  });
+});
+
+app.delete('/deleteInstance/:instanceIndex', (req, res) => {
+  const instanceIndex = req.params.instanceIndex;
+  if (instances[instanceIndex]) {
+    instances.splice(instanceIndex, 1);
+    res.sendStatus(204);
+  } else {
+    res.status(404).json({ success: false, message: 'Instância não encontrada' });
+  }
+});
+
+app.get('/listInstances', (req, res) => {
+  res.status(200).json({ success: true, instances });
+});
+
+app.use((req, res) => {
+  res.status(404).json({ success: false, message: 'Endpoint não encontrado' });
+});
+
+app.listen(80, () => {
+  console.log('API em execução na porta 3000');
 });
