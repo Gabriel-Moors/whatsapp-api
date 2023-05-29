3be2b84abc29f984924a38d428f51d073908b72a
Seu Nome <gabrielmoraes.plus@gmail.com>
2023-05-29 11:41:44 -0300

    Update app.js

--- app.js
+++ app.js
@@ -18,14 +18,6 @@ app.use(express.urlencoded({
   extended: true
 }));
 
-/**
- * BASEADO EM VÁRIAS PERGUNTAS
- * Mencionado nos tutoriais disponíveis
- *
- * Os dois middlewares acima lidam apenas com dados JSON e urlencode (x-www-form-urlencoded)
- * Portanto, precisamos adicionar um middleware extra para lidar com form-data
- * Aqui podemos usar o express-fileupload
- */
 app.use(fileUpload({
   debug: false
 }));
@@ -85,7 +77,7 @@ const createSession = function(id, description, webhookUrl) {
       clientId: id
     }),
     sessionData: {
-      webhookUrl: webhookUrl // Define a URL do webhook na sessão
+      webhookUrl: webhookUrl
     }
   });
 
@@ -93,11 +85,10 @@ const createSession = function(id, description, webhookUrl) {
 
   client.on('qr', (qr) => {
     console.log('QR RECEBIDO', qr);
-    const qrCode = 'data:image/png;base64,' + qr;
-    const sessionIndex = sessions.findIndex(sess => sess.id === id);
-    sessions[sessionIndex].qrCode = qrCode;
-    io.emit('qr', { id: id, src: qrCode });
-    io.emit('message', { id: id, text: 'QR Code recebido, por favor escaneie!' });
+    qrcode.toDataURL(qr, (err, url) => {
+      io.emit('qr', { id: id, src: url });
+      io.emit('message', { id: id, text: 'QR Code recebido, por favor escaneie!' });
+    });
   });
 
   client.on('ready', () => {
@@ -124,7 +115,6 @@ const createSession = function(id, description, webhookUrl) {
     client.destroy();
     client.initialize();
 
-    // Removendo do arquivo de sessões
     const savedSessions = getSessionsFile();
     const sessionIndex = savedSessions.findIndex(sess => sess.id == id);
     savedSessions.splice(sessionIndex, 1);
@@ -133,14 +123,12 @@ const createSession = function(id, description, webhookUrl) {
     io.emit('remove-session', id);
   });
 
-  // Adicione o cliente às sessões
   sessions.push({
     id: id,
     description: description,
     client: client
   });
 
-  // Adicionando a sessão no arquivo
   const savedSessions = getSessionsFile();
   const sessionIndex = savedSessions.findIndex(sess => sess.id == id);
 
@@ -149,7 +137,7 @@ const createSession = function(id, description, webhookUrl) {
       id: id,
       description: description,
       ready: false,
-      webhookUrl: webhookUrl // Adiciona a URL do webhook na sessão
+      webhookUrl: webhookUrl
     });
     setSessionsFile(savedSessions);
   }
@@ -160,13 +148,6 @@ const init = function(socket) {
 
   if (savedSessions.length > 0) {
     if (socket) {
-      /**
-       * Na primeira vez que é executado (por exemplo, ao reiniciar o servidor), nosso cliente ainda não está pronto!
-       * Ele precisará de algum tempo para autenticação.
-       * 
-       * Portanto, para evitar confusão com o status 'pronto',
-       * precisamos definir como FALSO para essa condição.
-       */
       savedSessions.forEach((e, i, arr) => {
         arr[i].ready = false;
       });
@@ -211,7 +192,7 @@ app.post('/create-session', (req, res) => {
   });
 });
 
-// Rota de Enviar Mensagem de Texto
+// Rota de Envio de Mensagem de Texto
 app.post('/send-message', async (req, res) => {
   console.log(req);
 
@@ -222,72 +203,32 @@ app.post('/send-message', async (req, res) => {
   const client = sessions.find(sess => sess.id == sender)?.client;
 
   if (!client) {
-    return res.status(404).json({
+    return res.status(422).json({
       status: false,
-      message: 'Sessão não encontrada.'
+      message: `O remetente: ${sender} não foi encontrado!`
+    })
+  }
+
+  const isRegisteredNumber = await client.isRegisteredUser(number);
+
+  if (!isRegisteredNumber) {
+    return res.status(422).json({
+      status: false,
+      message: 'O número não está registrado!'
     });
   }
 
-  try {
-    await client.sendMessage(number, message);
-
-    return res.status(200).json({
+  client.sendMessage(number, message).then(response => {
+    res.status(200).json({
       status: true,
-      message: 'Mensagem enviada com sucesso.'
+      response: response
     });
-  } catch (error) {
-    return res.status(500).json({
+  }).catch(err => {
+    res.status(500).json({
       status: false,
-      message: 'Erro ao enviar a mensagem: ' + error
+      response: err
     });
-  }
-});
-
-// Rota de Envio de Mídia
-app.post('/send-media', async (req, res) => {
-  const sender = req.body.sender;
-  const number = phoneNumberFormatter(req.body.number);
-  const caption = req.body.caption;
-
-  const client = sessions.find(sess => sess.id == sender)?.client;
-
-  if (!client) {
-    return res.status(404).json({
-      status: false,
-      message: 'Sessão não encontrada.'
-    });
-  }
-
-  if (!req.files || Object.keys(req.files).length === 0) {
-    return res.status(400).json({
-      status: false,
-      message: 'Nenhum arquivo foi enviado.'
-    });
-  }
-
-  try {
-    const media = new MessageMedia(
-      req.files.file.mimetype,
-      req.files.file.data.toString('base64'),
-      req.files.file.name
-    );
-
-    if (caption) {
-      await client.sendMessage(number, media, { caption: caption });
-    } else {
-      await client.sendMessage(number, media);
-    }
-
-    return res.status(200).json({
-      status: true,
-      message: 'Mídia enviada com sucesso.'
-    });
-  } catch (error) {
-    return res.status(500).json({
-      status: false,
-      message: 'Erro ao enviar a mídia: ' + error
-    });
-  }
+  });
 });
 
 // Rota de Geração de QR Code
@@ -302,16 +243,16 @@ app.get('/qr-code/:sessionId', (req, res) => {
     });
   }
 
-  if (!session.qrCode) {
-    return res.status(404).json({
-      status: false,
-      message: 'QR code não encontrado para esta sessão.'
-    });
-  }
+  const client = session.client;
 
-  res.send(session.qrCode);
+  client.on('qr', (qr) => {
+    res.json({
+      status: true,
+      qr: qr
+    });
+  });
 });
 
 server.listen(port, function() {
-  console.log('App ouvindo na porta ' + port);
+  console.log('App listening on *:' + port);
 });
