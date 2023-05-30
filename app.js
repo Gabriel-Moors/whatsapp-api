94cf45c85854936bf8691cc0149c4a1a78cd04eb
Seu Nome <gabrielmoraes.plus@gmail.com>
2023-05-29 22:10:22 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,55 +4,68 @@ const socketIO = require('socket.io');
 const qrcode = require('qrcode');
 const http = require('http');
 const fs = require('fs');
+const { phoneNumberFormatter } = require('./helpers/formatter');
 const fileUpload = require('express-fileupload');
 const axios = require('axios');
-const { phoneNumberFormatter } = require('./helpers/formatter');
+const port = process.env.PORT || 8000;
 
-const port = process.env.PORT || 80;
-const SESSIONS_FILE = './whatsapp-sessions.json';
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
-const sessions = [];
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(fileUpload({ debug: false }));
+app.use(express.urlencoded({
+  extended: true
+}));
 
-// Rota inicial
+/**
+ * BASED ON MANY QUESTIONS
+ * Actually ready mentioned on the tutorials
+ * 
+ * The two middlewares above only handle for data json & urlencode (x-www-form-urlencoded)
+ * So, we need to add extra middleware to handle form-data
+ * Here we can use express-fileupload
+ */
+app.use(fileUpload({
+  debug: false
+}));
+
 app.get('/', (req, res) => {
-  res.sendFile('index.html', { root: __dirname });
+  res.sendFile('index-multiple-account.html', {
+    root: __dirname
+  });
 });
 
-// Verifica se o arquivo de sessões existe, caso contrário, cria um novo
-const createSessionsFileIfNotExists = () => {
+const sessions = [];
+const SESSIONS_FILE = './whatsapp-sessions.json';
+
+const createSessionsFileIfNotExists = function() {
   if (!fs.existsSync(SESSIONS_FILE)) {
     try {
       fs.writeFileSync(SESSIONS_FILE, JSON.stringify([]));
-      console.log('Arquivo de sessões criado com sucesso.');
-    } catch (err) {
-      console.log('Falha ao criar o arquivo de sessões:', err);
+      console.log('Sessions file created successfully.');
+    } catch(err) {
+      console.log('Failed to create sessions file: ', err);
     }
   }
-};
+}
 
-// Salva as sessões no arquivo
-const setSessionsFile = (sessions) => {
-  fs.writeFile(SESSIONS_FILE, JSON.stringify(sessions), (err) => {
+createSessionsFileIfNotExists();
+
+const setSessionsFile = function(sessions) {
+  fs.writeFile(SESSIONS_FILE, JSON.stringify(sessions), function(err) {
     if (err) {
       console.log(err);
     }
   });
-};
+}
 
-// Retorna as sessões do arquivo
-const getSessionsFile = () => {
+const getSessionsFile = function() {
   return JSON.parse(fs.readFileSync(SESSIONS_FILE));
-};
+}
 
-// Cria uma nova sessão
-const createSession = (id, description, webhooks) => {
-  console.log('Criando sessão:', id);
+const createSession = function(id, description) {
+  console.log('Creating session: ' + id);
   const client = new Client({
     restartOnAuthFail: true,
     puppeteer: {
@@ -64,7 +77,7 @@ const createSession = (id, description, webhooks) => {
         '--disable-accelerated-2d-canvas',
         '--no-first-run',
         '--no-zygote',
-        '--single-process',
+        '--single-process', // <- this one doesn't works in Windows
         '--disable-gpu'
       ],
     },
@@ -76,16 +89,16 @@ const createSession = (id, description, webhooks) => {
   client.initialize();
 
   client.on('qr', (qr) => {
-    console.log('QR RECEBIDO:', qr);
+    console.log('QR RECEIVED', qr);
     qrcode.toDataURL(qr, (err, url) => {
       io.emit('qr', { id: id, src: url });
-      io.emit('message', { id: id, text: 'QR Code recebido, por favor, faça a leitura!' });
+      io.emit('message', { id: id, text: 'QR Code received, scan please!' });
     });
   });
 
   client.on('ready', () => {
     io.emit('ready', { id: id });
-    io.emit('message', { id: id, text: 'Whatsapp está pronto!' });
+    io.emit('message', { id: id, text: 'Whatsapp is ready!' });
 
     const savedSessions = getSessionsFile();
     const sessionIndex = savedSessions.findIndex(sess => sess.id == id);
@@ -95,18 +108,19 @@ const createSession = (id, description, webhooks) => {
 
   client.on('authenticated', () => {
     io.emit('authenticated', { id: id });
-    io.emit('message', { id: id, text: 'Whatsapp está autenticado!' });
+    io.emit('message', { id: id, text: 'Whatsapp is authenticated!' });
   });
 
-  client.on('auth_failure', () => {
-    io.emit('message', { id: id, text: 'Falha na autenticação, reiniciando...' });
+  client.on('auth_failure', function() {
+    io.emit('message', { id: id, text: 'Auth failure, restarting...' });
   });
 
   client.on('disconnected', (reason) => {
-    io.emit('message', { id: id, text: 'Whatsapp está desconectado!' });
+    io.emit('message', { id: id, text: 'Whatsapp is disconnected!' });
     client.destroy();
     client.initialize();
 
+    // Menghapus pada file sessions
     const savedSessions = getSessionsFile();
     const sessionIndex = savedSessions.findIndex(sess => sess.id == id);
     savedSessions.splice(sessionIndex, 1);
@@ -115,13 +129,14 @@ const createSession = (id, description, webhooks) => {
     io.emit('remove-session', id);
   });
 
+  // Tambahkan client ke sessions
   sessions.push({
     id: id,
     description: description,
-    client: client,
-    webhooks: webhooks
+    client: client
   });
 
+  // Menambahkan session ke file
   const savedSessions = getSessionsFile();
   const sessionIndex = savedSessions.findIndex(sess => sess.id == id);
 
@@ -130,165 +145,95 @@ const createSession = (id, description, webhooks) => {
       id: id,
       description: description,
       ready: false,
-      webhooks: webhooks
     });
     setSessionsFile(savedSessions);
   }
-};
+}
 
-// Inicializa as sessões salvas
-const init = (socket) => {
+const init = function(socket) {
   const savedSessions = getSessionsFile();
 
   if (savedSessions.length > 0) {
     if (socket) {
+      /**
+       * At the first time of running (e.g. restarting the server), our client is not ready yet!
+       * It will need several time to authenticating.
+       * 
+       * So to make people not confused for the 'ready' status
+       * We need to make it as FALSE for this condition
+       */
       savedSessions.forEach((e, i, arr) => {
         arr[i].ready = false;
       });
 
       socket.emit('init', savedSessions);
+    } else {
+      savedSessions.forEach(sess => {
+        createSession(sess.id, sess.description);
+      });
     }
   }
-};
+}
 
-// Inicializa o socket
-io.on('connection', (socket) => {
+init();
+
+// Socket IO
+io.on('connection', function(socket) {
   init(socket);
 
-  socket.on('create-session', (data) => {
-    console.log('Criando sessão:', data.id);
-    createSession(data.id, data.description, data.webhooks);
+  socket.on('create-session', function(data) {
+    console.log('Create session: ' + data.id);
+    createSession(data.id, data.description);
   });
 });
 
-// Rota para criar uma nova sessão
-app.post('/create-session', (req, res) => {
-  const id = req.body.id;
-  const description = req.body.description;
-  const webhooks = req.body.webhooks;
-
-  if (!id || !description || !webhooks || webhooks.length !== 4) {
-    return res.status(422).json({
-      status: false,
-      message: 'Os dados da sessão são inválidos ou estão faltando.'
-    });
-  }
-
-  createSession(id, description, webhooks);
-
-  return res.status(200).json({
-    status: true,
-    message: 'Sessão criada com sucesso.'
-  });
-});
-
-// Rota para deletar uma sessão
-app.delete('/delete-session/:id', (req, res) => {
-  const id = req.params.id;
-
-  const sessionIndex = sessions.findIndex(sess => sess.id == id);
-  if (sessionIndex == -1) {
-    return res.status(422).json({
-      status: false,
-      message: 'A sessão não existe.'
-    });
-  }
-
-  sessions.splice(sessionIndex, 1);
-
-  const savedSessions = getSessionsFile();
-  const savedSessionIndex = savedSessions.findIndex(sess => sess.id == id);
-  savedSessions.splice(savedSessionIndex, 1);
-  setSessionsFile(savedSessions);
-
-  return res.status(200).json({
-    status: true,
-    message: 'Sessão deletada com sucesso.'
-  });
-});
-
-// Rota para envio de mensagens
+// Send message
 app.post('/send-message', async (req, res) => {
+  console.log(req);
+
   const sender = req.body.sender;
   const number = phoneNumberFormatter(req.body.number);
   const message = req.body.message;
 
   const client = sessions.find(sess => sess.id == sender)?.client;
 
+  // Make sure the sender is exists & ready
   if (!client) {
     return res.status(422).json({
       status: false,
-      message: `O remetente ${sender} não foi encontrado!`
-    });
+      message: `The sender: ${sender} is not found!`
+    })
   }
 
+  /**
+   * Check if the number is already registered
+   * Copied from app.js
+   * 
+   * Please check app.js for more validations example
+   * You can add the same here!
+   */
   const isRegisteredNumber = await client.isRegisteredUser(number);
 
   if (!isRegisteredNumber) {
     return res.status(422).json({
       status: false,
-      message: 'O número não está registrado'
+      message: 'The number is not registered'
     });
   }
 
-  client.sendMessage(number, message)
-    .then(response => {
-      res.status(200).json({
-        status: true,
-        response: response
-      });
-    })
-    .catch(err => {
-      res.status(500).json({
-        status: false,
-        response: err
-      });
+  client.sendMessage(number, message).then(response => {
+    res.status(200).json({
+      status: true,
+      response: response
     });
+  }).catch(err => {
+    res.status(500).json({
+      status: false,
+      response: err
+    });
+  });
 });
 
-// Rota para enviar arquivo de mídia
-app.post('/send-media', async (req, res) => {
-  const sender = req.body.sender;
-  const number = phoneNumberFormatter(req.body.number);
-  const caption = req.body.caption;
-  const fileUrl = req.body.file;
-
-  const client = sessions.find(sess => sess.id == sender)?.client;
-
-  if (!client) {
-    return res.status(422).json({
-      status: false,
-      message: `O remetente ${sender} não foi encontrado!`
-    });
-  }
-
-  const isRegisteredNumber = await client.isRegisteredUser(number);
-
-  if (!isRegisteredNumber) {
-    return res.status(422).json({
-      status: false,
-      message: 'O número não está registrado'
-    });
-  }
-
-  const media = MessageMedia.fromUrl(fileUrl);
-
-  client.sendMessage(number, media, { caption: caption })
-    .then(response => {
-      res.status(200).json({
-        status: true,
-        response: response
-      });
-    })
-    .catch(err => {
-      res.status(500).json({
-        status: false,
-        response: err
-      });
-    });
-});
-
-// Inicia o servidor
-server.listen(port, function () {
-  console.log('App está executando na porta ' + port);
+server.listen(port, function() {
+  console.log('App running on *: ' + port);
 });
